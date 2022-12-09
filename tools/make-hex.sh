970d2972d5bf07f8e3362aab51d09c20b73b75b4
Laris Qiao <laris.qiao@gmail.com>
2022-12-10 00:49:31 +0800

    fix path

--- tools/make-hex.sh
+++ tools/make-hex.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-avr-objcopy -S -j .text -j .data -O ihex target/avr-atmega328p/debug/*.elf ../target/avr-atmega328p/debug/1.hex
-avr-objcopy -S -j .text -j .data -O ihex target/avr-atmega328p/release/*.elf ../target/avr-atmega328p/release/1.hex
+avr-objcopy -S -j .text -j .data -O ihex target/avr-atmega328p/debug/*.elf target/avr-atmega328p/debug/1.hex
+avr-objcopy -S -j .text -j .data -O ihex target/avr-atmega328p/release/*.elf target/avr-atmega328p/release/1.hex
 
 #BYTES=$(avr-size "$ELF" | tail -1 | cut -f4 | bc)
 #echo "$ELF:" >&2
